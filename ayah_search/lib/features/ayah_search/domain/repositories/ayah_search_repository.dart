af702466c912b72b16795765d66c61ec76bda4d1
Prantik <coderprantik@gmai.com>
2021-09-03 07:32:33 +0600

    ayah_search_repository created

---
+++ ayah_search/lib/features/ayah_search/domain/repositories/ayah_search_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failure.dart';
+import '../entities/ayah.dart';
+
+abstract class AyahSearchRepository {
+  Future<Either<Failure, Ayah>> getArabicAyah(String query);
+  Future<Either<Failure, Ayah>> getTranslationAyah(String query);
+}
