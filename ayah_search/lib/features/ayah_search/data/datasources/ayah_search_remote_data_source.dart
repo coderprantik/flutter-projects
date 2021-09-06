cff65299e6ad5865bf52739323c969cb135c4fa8
Prantik <coderprantik@gmai.com>
2021-09-06 08:46:58 +0600

    remote data source - getArabicAyah all tests passed

--- ayah_search/lib/features/ayah_search/data/datasources/ayah_search_remote_data_source.dart
+++ ayah_search/lib/features/ayah_search/data/datasources/ayah_search_remote_data_source.dart
@@ -1,5 +1,6 @@
+import 'package:ayah_search/core/error/exceptions.dart';
 import 'package:ayah_search/features/ayah_search/data/models/ayah_model.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart';
 
 abstract class AyahSearchRemoteDataSource {
   Future<AyahModel> getArabicAyah({
@@ -13,14 +14,26 @@ abstract class AyahSearchRemoteDataSource {
 }
 
 class AyahSearchRemoteDataSourceImpl implements AyahSearchRemoteDataSource {
-  final Dio client;
+  final Client client;
+  final _endPoint = 'https://api.alquran.cloud/v1/ayah';
+  final _headers = {'Content-Type': 'application/json'};
 
   AyahSearchRemoteDataSourceImpl({required this.client});
 
   @override
-  Future<AyahModel> getArabicAyah({required String query}) {
-    // TODO: implement getArabicAyah
-    throw UnimplementedError();
+  Future<AyahModel> getArabicAyah({required String query}) async {
+    final response = await client.get(
+      Uri.parse('$_endPoint/$query'),
+      headers: _headers,
+    );
+
+    if (response.statusCode == 200) {
+      return AyahModel.fromRawJson(response.body);
+    } else if (response.statusCode == 404) {
+      throw ServerException(response.body);
+    } else {
+      throw ServerException(response.reasonPhrase ?? 'Something went wrong!');
+    }
   }
 
   @override
--- ayah_search/test/features/ayah_search/data/datasources/ayah_search_remote_data_source_impl_test.dart
+++ ayah_search/test/features/ayah_search/data/datasources/ayah_search_remote_data_source_impl_test.dart
@@ -1,15 +1,112 @@
+import 'dart:io' as io;
+
+import 'package:ayah_search/core/error/exceptions.dart';
 import 'package:ayah_search/features/ayah_search/data/datasources/ayah_search_remote_data_source.dart';
+import 'package:ayah_search/features/ayah_search/data/models/ayah_model.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-import 'dio.mocks.dart';
+import '../../../../fixtures/fixture_reader.dart';
+import 'http_client.mocks.dart';
 
-// @GenerateMocks([Dio])
+@GenerateMocks([Client])
 void main() {
   late AyahSearchRemoteDataSourceImpl dataSource;
-  late MockDio mockClient;
+  late MockClient mockClient;
 
   setUp(() {
-    mockClient = MockDio();
+    mockClient = MockClient();
     dataSource = AyahSearchRemoteDataSourceImpl(client: mockClient);
+  });
+
+  final endPoint = 'https://api.alquran.cloud/v1/ayah';
+  final headers = {'Content-Type': 'application/json'};
+
+  final tQuery = '1:2';
+  final jsonString = fixture('ayah_remote.json');
+  final tAyahModel = AyahModel.fromRawJson(jsonString);
+
+  void setUpMockClientSuccess200() {
+    when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+      (_) async => Response(
+        jsonString,
+        200,
+        headers: {
+          io.HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8',
+        },
+      ),
+    );
+  }
+
+  void setUpMockClientFailure404() {
+    when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+      (_) async => Response('No data found!', 404),
+    );
+  }
+
+  group('getArabicAyah', () {
+    test(
+      '''should perform a GET request on a URL with query
+      being the endpoint and with application/json header''',
+      () async {
+        // arrange
+        setUpMockClientSuccess200();
+        // act
+        await dataSource.getArabicAyah(query: tQuery);
+        // assert
+        verify(mockClient.get(
+          Uri.parse('$endPoint/$tQuery'),
+          headers: headers,
+        ));
+      },
+    );
+
+    test(
+      'should return Ayah Model when the response code is 200 (success)',
+      () async {
+        // arrange
+        setUpMockClientSuccess200();
+        // act
+        final result = await dataSource.getArabicAyah(query: tQuery);
+        // assert
+        expect(result, equals(tAyahModel));
+      },
+    );
+
+    test(
+      '''should throw a ServerException with message
+       when the response code is 404 or other (failure)''',
+      () async {
+        // arrange
+        setUpMockClientFailure404();
+        // act
+        final call = dataSource.getArabicAyah;
+        // assert
+        expect(
+          () => call(query: tQuery),
+          throwsA(TypeMatcher<ServerException>()),
+        );
+      },
+    );
+
+    test(
+      'should throw SocketException when there is no internet',
+      () async {
+        // arrange
+        when(mockClient.get(
+          Uri.parse('$endPoint/$tQuery'),
+          headers: anyNamed('headers'),
+        )).thenThrow(io.SocketException('No Inernet'));
+        // act
+        final call = dataSource.getArabicAyah;
+        // assert
+        expect(
+          () => call(query: tQuery),
+          throwsA(TypeMatcher<io.SocketException>()),
+        );
+      },
+    );
   });
 }
