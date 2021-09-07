fe392778a6c2b82954907f693c27166417f38862
Prantik <coderprantik@gmai.com>
2021-09-07 10:33:51 +0600

    get pages

---
+++ ayah_search/lib/app/router/app_pages.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+part './app_routes.dart';
+
+abstract class AppPages {
+
+  static final pages = [
+    GetPage(name: Routes.HOME, page:()=> MyHomePage(),)
+  ];
+}
--- ayah_search/lib/injection_container.dart
+++ ayah_search/lib/injection_container.dart
@@ -1,17 +1,18 @@
-import 'package:ayah_search/core/network/network_info.dart';
-import 'package:ayah_search/core/utils/input_formatter.dart';
-import 'package:ayah_search/features/ayah_search/data/datasources/ayah_search_local_data_source.dart';
-import 'package:ayah_search/features/ayah_search/data/datasources/ayah_search_remote_data_source.dart';
-import 'package:ayah_search/features/ayah_search/data/repositories/ayah_search_repository_impl.dart';
-import 'package:ayah_search/features/ayah_search/domain/repositories/ayah_search_repository.dart';
-import 'package:ayah_search/features/ayah_search/domain/usecases/get_arabic_ayah.dart';
-import 'package:ayah_search/features/ayah_search/domain/usecases/get_translation_ayah.dart';
-import 'package:ayah_search/features/ayah_search/presentation/controller/ayah_search_controller.dart';
 import 'package:connection_checker/connection_checker.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/network/network_info.dart';
+import 'core/utils/input_formatter.dart';
+import 'features/ayah_search/data/datasources/ayah_search_local_data_source.dart';
+import 'features/ayah_search/data/datasources/ayah_search_remote_data_source.dart';
+import 'features/ayah_search/data/repositories/ayah_search_repository_impl.dart';
+import 'features/ayah_search/domain/repositories/ayah_search_repository.dart';
+import 'features/ayah_search/domain/usecases/get_arabic_ayah.dart';
+import 'features/ayah_search/domain/usecases/get_translation_ayah.dart';
+import 'features/ayah_search/presentation/controller/ayah_search_controller.dart';
 
 final locator = GetIt.instance;
 
